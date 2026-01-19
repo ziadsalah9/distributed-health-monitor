a41fbcdd886947619499e23065b9e43bd1f6fb7a
ziadsalah9 <105482551+ziadsalah9@users.noreply.github.com>
2026-01-19 03:04:21 +0200

    before refactor clean arch

---
+++ internal/worker/worker.go
@@ -0,0 +1,95 @@
+package worker
+
+import (
+    "net/http"
+	"distributed-health-monitor/internal/db"
+	"distributed-health-monitor/internal/models"
+	"log"
+	"strconv"
+	"time"
+	
+amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func StartWorker(c*amqp.Connection){
+
+	ch,err:=c.Channel()
+	if err!=nil{
+		log.Fatalf("Failed to open a channel: %v", err)
+	}
+	defer ch.Close()
+	
+	q,err:= ch.QueueDeclare("health_checks",true,false,false,false,nil)
+
+	msg,err :=ch.Consume(q.Name,"",true,false,false,false,nil)
+
+	log.Println(" Worker is waiting for messages...")
+
+
+	for d:=range msg{
+
+     serviceId,_ := strconv.Atoi(string(d.Body))
+	 log.Printf("Received a message for Service ID: %d", serviceId)
+
+	 processCheck(uint(serviceId))
+	}
+
+
+
+}
+
+
+func processCheck(id uint) {
+
+
+	var service models.Service
+
+	if err := db.DB.First(&service,id).Error; err != nil {
+		log.Printf("Service with ID %d not found", id)
+		return
+	}
+
+	timeout := 10 // default timeout
+	if service.Timeout > 0 {
+		timeout = service.Timeout
+	}
+
+	client := http.Client{ 
+		Timeout: time.Duration(timeout) * time.Second,
+	}
+
+	start := time.Now()
+	resp, err := client.Get(service.URL)  // Perform HTTP GET request statuscode : 2** or 4**
+	latency := time.Since(start).Milliseconds()  // now() - start
+
+	status := "DOWN"
+	statusCode:= "Error"
+
+	
+	if err == nil {
+		statusCode = resp.Status
+		if resp.StatusCode >= 200 && resp.StatusCode < 300 {
+			status = "UP"
+		}
+		resp.Body.Close()
+	}
+
+	db.DB.Model(&service).Updates(map[string]interface{}{
+		"last_status": status,
+		"last_check":  time.Now(),
+	})
+
+	healthLog := models.HealthLog{
+		ServiceID: service.ID,
+		Status:    status,
+		LatencyMs: int(latency),
+		CheckedAt: time.Now(),
+	}
+	db.DB.Create(&healthLog)
+
+log.Printf("Finished check for %s: Result [%s], Latency [%dms]", service.Name, status, latency )
+
+log.Printf("Status Code: %s", statusCode)
+
+}
+
